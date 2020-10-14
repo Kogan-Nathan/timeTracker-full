c89ea898cd0130eab8d965ae62c9128792f011f1
nathan <kogannathan@gmail.com>
2020-10-14 12:38:23 +0300

    delete duplicate

--- front/src/Reducers/index.jsx
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-import isLoggedReducer from './isLogged'
-
-
-const allReducers = combineReducers({
-    isLogged : isLoggedReducer,
-});
-
-export default allReducers;
